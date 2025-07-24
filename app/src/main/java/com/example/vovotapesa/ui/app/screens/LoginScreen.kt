afdf6723c76f98a80c9dbc10d75cd7adcafe29b8
APDYd-dotcom <arakazaprincedestinyvan@gmail.com>
2025-07-24 17:20:17 +0200

    LoginScreen Design final

--- app/src/main/java/com/example/vovotapesa/ui/app/screens/LoginScreen.kt
+++ app/src/main/java/com/example/vovotapesa/ui/app/screens/LoginScreen.kt
@@ -10,17 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -29,7 +24,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import com.example.vovotapesa.R
 import com.example.vovotapesa.ui.app.components.HeaderTextComponent
@@ -37,15 +31,13 @@ import com.example.vovotapesa.ui.app.components.MediumTextComponent
 import com.example.vovotapesa.ui.app.components.MyPasswordTextField
 import com.example.vovotapesa.ui.app.components.MyTextFieldComponent
 import com.example.vovotapesa.ui.app.components.NormalTextComponent
-import java.nio.file.WatchEvent
 
 @Composable
 fun LoginScreen(onLoginClick:()-> Unit, onSignupClick:()-> Unit){
   val ( email, setEmail) = rememberSaveable { mutableStateOf("") }
   val ( password, setPassword) = rememberSaveable { mutableStateOf("") }
-  Scaffold(
 
-  ) { innerppading ->
+  Scaffold { innerppading ->
     Column(
       modifier = Modifier.fillMaxSize()
         .padding(innerppading)
