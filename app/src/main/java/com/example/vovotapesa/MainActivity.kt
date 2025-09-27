23a702d2dfa5a3dfeb30c507415c2ac55a6990a0
APDYd-dotcom <arakazaprincedestinyvan@gmail.com>
2025-09-27 20:04:37 +0200

    backUp

--- app/src/main/java/com/example/vovotapesa/MainActivity.kt
+++ app/src/main/java/com/example/vovotapesa/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.vovotapesa.ui.app.components.CountryDropdownWithFlags
 import com.example.vovotapesa.ui.app.navigation.App
 import com.example.vovotapesa.ui.theme.VovotapesaTheme
 import dagger.hilt.android.AndroidEntryPoint
